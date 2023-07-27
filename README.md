415c3ef9ead2805476fc78953bc1b8e12bb19910
Velorian-TDV <terleckydima92@gmail.com>
2023-07-27 20:54:55 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # goit-react-hw-03-image-finder
+### GitHub Pages: https://velorian-tdv.github.io/goit-react-hw-03-image-finder/
---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="App">
+            Initial commit
+        </div>
+    );
+}
+
+export default App;
